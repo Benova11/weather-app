03c0c3c6f4bb6ac3bdc621c1a0e7ddc41d9d51e0
Ben Efrat <34989765+Benova11@users.noreply.github.com>
2020-03-16 20:39:04 +0200

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
   const url =
     'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
     encodeURIComponent(address) +
-    '.json?access_token=pk.eyJ1IjoiYmVub3ZhMTEiLCJhIjoiY2s2aHp4OWZ2MWZ0bjNsbnZoOTFlZGNwbyJ9.ZS54jBavMhrSBEoFRRfHHw&limit=1';
+    '.json?access_token=*************&limit=1';
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback(error, undefined);
